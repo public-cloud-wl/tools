dd6887b3081af54693e3272dc72cf54679e30257
Maarc-D <escasgorille@aol.com>
2024-10-24 14:42:25 +0200

    chore(init): fix utils

---
+++ utils/retry.go
@@ -0,0 +1,21 @@
+package utils
+
+import (
+	"time"
+)
+
+func Retry(toRetry func() error) error {
+	n := 0
+	var err error
+	for n < 10 {
+		err = toRetry()
+		if err == nil {
+			break
+		}
+		n++
+		log.Printf("[DEBUG] Issue on request retry: %d, -> try again", n)
+		sleepDuration, _ := time.ParseDuration(fmt.Sprintf("%ds", 2*n))
+		time.Sleep(sleepDuration)
+	}
+	return err
+}
